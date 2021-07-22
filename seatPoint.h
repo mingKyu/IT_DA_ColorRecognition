b19174f9818bf76d85cc8a495287afc9cf1786ff
DESKTOP-BJ3736H\강민규 <forthepawnofkang@gmail.com>
2021-07-22 14:44:20 +0900

    basis

---
+++ seatPoint.h
@@ -0,0 +1,13 @@
+//좌석의 최대 개수와 위치점들을 표기하는 헤더입니다.
+
+#define MAXSEAT 20
+
+#define SEAT1xT 260
+#define SEAT1xB 180
+#define SEAT1yT 180
+#define SEAT1yB 80
+
+#define SEAT2xT 490																																						
+#define SEAT2xB 390
+#define SEAT2yT 170
+#define SEAT2yB 80
