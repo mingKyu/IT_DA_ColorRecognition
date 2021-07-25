2820c58886ffef7f23e5951cf55fb4da9cc1e95c
RENTAL-3\SW교육지원팀 <forthepawnofkang@gmail.com>
2021-07-25 20:49:46 +0900

    reset seat point

--- seatPoint.h
+++ seatPoint.h
@@ -2,12 +2,12 @@
 
 #define MAXSEAT 20
 
-#define SEAT1xT 260
-#define SEAT1xB 180
-#define SEAT1yT 180
-#define SEAT1yB 80
+#define SEAT1xT 235
+#define SEAT1xB 100
+#define SEAT1yT 390
+#define SEAT1yB 330
 
-#define SEAT2xT 490																																						
-#define SEAT2xB 390
-#define SEAT2yT 170
-#define SEAT2yB 80
+#define SEAT2xT 380																																						
+#define SEAT2xB 280
+#define SEAT2yT 265
+#define SEAT2yB 245
